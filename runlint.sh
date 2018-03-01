8550bcccb759f416fddf658c79fcdff707aeee5a
MattatG3 <matt.jenks@griffingroupglobal.com>
2018-03-01 13:02:19 -0500

    GP-150 update runlint to pick up app.js

--- runlint.sh
+++ runlint.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 
-npm run lint -- ./src/src/**/*.js && npm run lint -- ./src/test/**/*.spec.js
+npm run lint -- ./src/src/*.js &&
+npm run lint -- ./src/src/**/*.js &&
+npm run lint -- ./src/test/**/*.spec.js
